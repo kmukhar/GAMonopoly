c58f5a466d1e09a1eecaca78a18e8920ede145c9
Kevin <kevin.mukhar@gmail.com>
2013-02-26 10:33:54 -0700

    clean up "About" dialog

--- Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/play2/PlayerGui.java
+++ Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/play2/PlayerGui.java
@@ -5,7 +5,10 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.*;
 import java.util.logging.*;
+
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 import edu.uccs.ecgs.ga.*;
 import edu.uccs.ecgs.players.AbstractPlayer;
@@ -118,7 +121,6 @@ public class PlayerGui extends JPanel {
       path = "About2.html";
     }
     
-    JLabel label = new JLabel();
     InputStream is = PlayerGui.class.getResourceAsStream(path);
     InputStreamReader isr = new InputStreamReader(is);
     BufferedReader br = new BufferedReader(isr); 
@@ -143,9 +145,82 @@ public class PlayerGui extends JPanel {
       }
     }
 
-    label.setText(aboutMsg.toString());
+    System.setProperty("awt.useSystemAAFontSettings", "on");
+    final JEditorPane editorPane = new JEditorPane();
+    editorPane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES,
+        Boolean.TRUE);
+    editorPane.setPreferredSize(new Dimension(500,420));
+    editorPane.setEditable(false);
+    editorPane.setContentType("text/html");
+    editorPane.setText(aboutMsg.toString());
 
-    int result = JOptionPane.showConfirmDialog(null, label,
+    // This section of code from 
+    // https://forums.oracle.com/forums/message.jspa?messageID=9909614
+    Color c = new Color(214, 217, 223); // default color for JOptionPane
+    UIDefaults defaults = new UIDefaults();
+    defaults.put("EditorPane[Enabled].backgroundPainter", c);
+    editorPane.putClientProperty("Nimbus.Overrides", defaults);
+    editorPane.putClientProperty("Nimbus.Overrides.InheritDefaults", true);
+    editorPane.setBackground(c);
+    // end code https://forums.oracle.com/forums/message.jspa?messageID=9909614
+
+    // the code for resizing the editorpane is from 
+    // http://java.dzone.com/tips/tip-displaying-rich-messages-u
+    // set editor pane to be resizeable
+    editorPane.addHierarchyListener(new HierarchyListener() {
+      public void hierarchyChanged(HierarchyEvent e) {
+        Window window = SwingUtilities.getWindowAncestor(editorPane);
+        if (window instanceof Dialog) {
+          Dialog dialog = (Dialog) window;
+          if (!dialog.isResizable()) {
+            dialog.setResizable(true);
+          }
+        }
+      }
+    });
+
+    // the code for processing hyperlinks is from 
+    // http://java.dzone.com/tips/tip-displaying-rich-messages-u
+    // Add Hyperlink listener to process hyperlinks
+    editorPane.addHyperlinkListener(new HyperlinkListener() {
+      public void hyperlinkUpdate(final HyperlinkEvent e) {
+        if (e.getEventType() == HyperlinkEvent.EventType.ENTERED) {
+          EventQueue.invokeLater(new Runnable() {
+            public void run() {
+              // Show hand cursor
+              SwingUtilities.getWindowAncestor(editorPane).setCursor(
+                  Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+              // Show URL as the tooltip
+              editorPane.setToolTipText(e.getURL().toExternalForm());
+            }
+          });
+        } else if (e.getEventType() == HyperlinkEvent.EventType.EXITED) {
+          EventQueue.invokeLater(new Runnable() {
+            public void run() {
+              // Show default cursor
+              SwingUtilities.getWindowAncestor(editorPane).setCursor(
+                  Cursor.getDefaultCursor());
+              // Reset tooltip
+              editorPane.setToolTipText(null);
+            }
+          });
+        } else if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+          if (Desktop.isDesktopSupported()) {
+            try {
+              Desktop.getDesktop().browse(e.getURL().toURI());
+            } catch (Exception ignored) {
+            }
+          }
+        }
+      }
+    });
+
+    JScrollPane sp = new JScrollPane(editorPane);
+    sp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    sp.setBorder(null);
+
+    int result = JOptionPane.showConfirmDialog(null, sp,
         "About this program", JOptionPane.OK_CANCEL_OPTION,
         JOptionPane.INFORMATION_MESSAGE);
     
@@ -235,8 +310,10 @@ public class PlayerGui extends JPanel {
 
     JScrollPane scrollPane = new JScrollPane(gameInfo);
     scrollPane.setPreferredSize(new Dimension(180,50));
-    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    scrollPane
+        .setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+    scrollPane
+        .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     
     JTextField columnHead = new JTextField("Game Information");
     columnHead.setEditable(false);
