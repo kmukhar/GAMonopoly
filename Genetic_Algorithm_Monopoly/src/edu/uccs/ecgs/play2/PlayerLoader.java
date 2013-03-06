1b2122ebf5c9e8e5216ee006bd5afb103eec9638
Kevin <kevin.mukhar@gmail.com>
2013-03-06 12:37:19 -0700

    Changed process for loading players

--- Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/play2/PlayerLoader.java
+++ Genetic_Algorithm_Monopoly/src/edu/uccs/ecgs/play2/PlayerLoader.java
@@ -1,13 +1,41 @@
 package edu.uccs.ecgs.play2;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.Random;
+
 import edu.uccs.ecgs.players.AbstractPlayer;
 import edu.uccs.ecgs.players.ChromoTypes;
 
 public class PlayerLoader {
 
-	public static AbstractPlayer loadPlayer(String path, int index) {
-	  DataInputStream dis = null;
+  private static PlayerLoader _ref = new PlayerLoader();
+  private Properties playerFiles;
+  
+  private PlayerLoader() {
+    InputStream is = 
+        PlayerLoader.class.getResourceAsStream("data/playerfiles.properties");
+    playerFiles = new Properties();
+
+    try {
+      playerFiles.load(is);
+    } catch (IOException ignored) {
+    } finally {
+      try {
+        is.close();
+      } catch (IOException ignored) {
+      }
+    }
+  }
+
+  public static PlayerLoader getLoader() {
+    return _ref;
+  }
+
+  private AbstractPlayer loadPlayer(int index, String path) {
+    DataInputStream dis = null;
     AbstractPlayer player = null;
 
     try {
@@ -23,6 +51,7 @@ public class PlayerLoader {
 
       ChromoTypes chromoType = ChromoTypes.valueOf(headerStr);
       player = chromoType.getPlayer(index, dis);
+      player.setSourceName(path);
 
     } catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -39,5 +68,23 @@ public class PlayerLoader {
     }
     
     return player;
-	}	
+	}
+
+  public AbstractPlayer[] get4Players() {
+    AbstractPlayer[] players = new AbstractPlayer[4];
+
+    Collection<Object> vals = playerFiles.values();
+    ArrayList<Object> filenames = new ArrayList<Object>();
+    filenames.addAll(vals);
+
+    Random r = new Random(System.currentTimeMillis());
+
+    for (int i = 0; i < players.length; i++) {
+      int playerIndex = i + 1;
+      Object path = filenames.remove(r.nextInt(filenames.size()));
+      players[i] = this.loadPlayer(playerIndex, path.toString());
+    }
+
+    return players;
+  }	
 }
